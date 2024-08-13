a8a92e5a3d172f01baf37a14eb8ed5d163d3df7d
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-13 09:57:18 -0300

    bottom nav bar e paginas RatedMovie e HomePage

---
+++ lib/pages/home/home_page.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/models/movie_model.dart';
+import 'package:movie_app/pages/home/widgets/movies_horizontal_list.dart';
+import 'package:movie_app/pages/home/widgets/nowplaying_list.dart';
+import 'package:movie_app/services/api_services.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  ApiServices apiService = ApiServices();
+  List<Movie> nowPlaying = [];
+
+@override
+  void initState() {
+    nowPlaying = apiService.getMovies();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Movie App'),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                child: Text(
+                  'Now Playing',
+                  style: TextStyle(
+                    color: Colors.white54,
+                    fontWeight: FontWeight.w300,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              NowPlayingList(movies: nowPlaying),
+              const SizedBox(
+                height: 20,
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                child: Text(
+                  'Popular',
+                  style: TextStyle(
+                    color: Colors.white54,
+                    fontWeight: FontWeight.w300,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              MoviesHorizontalList(),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                child: Text(
+                  'Upcoming',
+                  style: TextStyle(
+                    color: Colors.white54,
+                    fontWeight: FontWeight.w300,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              MoviesHorizontalList(),
+              const SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/home/widgets/nowplaying_list.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/models/movie_model.dart';
+import 'package:movie_app/widgets/custom_card_thumbnail.dart';
+
+class NowPlayingList extends StatefulWidget {
+  final List<Movie> movies;
+  const NowPlayingList({super.key, required this.movies});
+
+  @override
+  State<NowPlayingList> createState() => _NowPlayingListState();
+}
+
+class _NowPlayingListState extends State<NowPlayingList> {
+  PageController pageController = PageController(viewportFraction: 0.8);
+  int currentPage = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.5,
+          child: PageView.builder(
+            controller: pageController,
+            itemCount: widget.movies.length,
+            itemBuilder: (context, index) {
+            return CustomCardThumbnail(imageAsset: widget.movies[index].backdropPath);
+          },),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: _buildPageIndicators(),
+        ),
+      ],
+    );
+  }
+
+  List<Widget> _buildPageIndicators() {
+    List<Widget> indicatorList = [];
+    for(int i = 0; i < widget.movies.length; i++){
+      if(i == currentPage){
+        indicatorList.add(_buildIndicator(true));
+      }else{
+        indicatorList.add(_buildIndicator(false));
+      }
+    }
+    return indicatorList;
+  }
+
+  Widget _buildIndicator(bool isActive) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 150),
+      margin: const EdgeInsets.symmetric(horizontal: 5.0),
+      height: 8.0,
+      width: 8.0,
+      decoration: BoxDecoration(
+        color: isActive ? Colors.white : Colors.white24,
+        borderRadius: BorderRadius.circular(20),
+      ),
+    );
+  }
+}
