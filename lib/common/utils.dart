4e720ec12cda32cbe8f554b118de5595c88db70a
Gabriel Guedes <101231820+gabguedes@users.noreply.github.com>
2024-09-22 18:17:33 -0300

    Update utils.dart

--- lib/common/utils.dart
+++ lib/common/utils.dart
@@ -4,4 +4,4 @@ const imageUrl = 'https://image.tmdb.org/t/p/w500';
 const kBackgoundColor = Color(0xff121012);
 const kbuttonColor = Color.fromARGB(255, 89, 54, 133);
 const Color backgroundPrimary = Color.fromARGB(255, 171, 169, 180);
-const apiKey = '0e1c4be889057d7821a5e851f2ebf671';
+const apiKey = '';
